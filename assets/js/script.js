ab5874c29fa31ccf92e4825d7b7da364c35945e0
Muddapu akash <120655408+Muddapuakash@users.noreply.github.com>
2024-09-04 00:13:02 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["AIML developer", "Full Stack development", "web designing", "Deep Learning", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
