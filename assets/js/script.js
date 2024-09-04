1fb4294034e6303255b2f34ef1d47ccf2d020216
Muddapu akash <120655408+Muddapuakash@users.noreply.github.com>
2024-09-04 19:51:02 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -208,14 +208,17 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_66d860cbea492f34bc0da1b1';
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/66d860cbea492f34bc0da1b1/1i6um5507';
+s1.src='https://embed.tawk.to/66d860cbea492f34bc0da1b1/1i6umeumn';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);})();
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 // End of Tawk.to Live Chat
 
 
